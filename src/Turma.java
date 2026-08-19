21f729a7ab1333653fe2fba00d64ddfcf4c451c3
Enzo Celuppi Sanzovo <enzo.celuppi15@gmail.com>
2026-08-19 16:29:57 -0300

    Update Turma.java

--- src/Turma.java
+++ src/Turma.java
@@ -9,10 +9,12 @@ public class Turma {
     }
 
     public void listarAlunos() {
-        if (alunos.isEmpty()){
+        if (alunos.isEmpty()) {
             System.out.println("Nenhum aluno cadastrado!");
+            return;
         }
-        for (int i = 0; i < alunos.size(); i++){
+
+        for (int i = 0; i < alunos.size(); i++) {
             System.out.println("---------------");
             System.out.println((i + 1) + " - " + alunos.get(i).getNome());
             System.out.println("---------------");
@@ -20,52 +22,70 @@ public class Turma {
     }
 
     public void adicionarNota(int qualAluno, ArrayList<Double> notas) {
-        if (qualAluno < 1 || qualAluno > alunos.size()){
+        if (qualAluno < 1 || qualAluno > alunos.size()) {
             System.out.println("Esse aluno não existe");
             return;
-        } else {
-            alunos.get(qualAluno - 1).setNotas(notas);
         }
 
+        alunos.get(qualAluno - 1).setNotas(notas);
     }
 
     public void calcularMedia(int alunoMedia) {
-        if (alunos.get(alunoMedia - 1).getNotas().isEmpty()){
-            System.out.println("Aluno não possui notas cadstradas!");
-        } else{
-            ArrayList<Double> media = alunos.get(alunoMedia - 1).getNotas();
-            double mediaFinal = (media.get(0) + media.get(1) + media.get(2) + media.get(3)) / 4;
 
-
-            System.out.println("---------------");
-            System.out.println(alunoMedia + " - " + alunos.get(alunoMedia - 1).getNome());
-            System.out.println("Média: " + mediaFinal);
-            System.out.println("---------------");
+        if (!existeAluno(alunoMedia)) {
+            System.out.println("Aluno não existe!");
+            return;
         }
+
+        if (alunos.get(alunoMedia - 1).getNotas().isEmpty()) {
+            System.out.println("Aluno não possui notas cadastradas!");
+            return;
+        }
+
+        ArrayList<Double> notas = alunos.get(alunoMedia - 1).getNotas();
+
+        double soma = 0;
+
+        for (double nota : notas) {
+            soma += nota;
+        }
+
+        double mediaFinal = soma / notas.size();
+
+        System.out.println("---------------");
+        System.out.println(alunoMedia + " - " + alunos.get(alunoMedia - 1).getNome());
+        System.out.println("Média: " + mediaFinal);
+        System.out.println("---------------");
     }
 
     public void verBoletim(int alunoBoletim) {
-        if (alunos.get(alunoBoletim - 1).getNotas().isEmpty()){
-            System.out.println("Aluno não possui notas cadstradas!");
-        } else{
-            double nota1 = alunos.get(alunoBoletim - 1).getNotas().get(0);
-            double nota2 = alunos.get(alunoBoletim - 1).getNotas().get(1);
-            double nota3 = alunos.get(alunoBoletim - 1).getNotas().get(2);
-            double nota4 = alunos.get(alunoBoletim - 1).getNotas().get(3);
 
-            System.out.println("---------------");
-            System.out.println(alunoBoletim +" - " + alunos.get(alunoBoletim - 1).getNome());
-            System.out.println("Nota 1: " + nota1);
-            System.out.println("Nota 2: " + nota2);
-            System.out.println("Nota 3: " + nota3);
-            System.out.println("Nota 4: " + nota4);
-            System.out.println("---------------");
+        if (!existeAluno(alunoBoletim)) {
+            System.out.println("Aluno não existe!");
+            return;
         }
+
+        if (alunos.get(alunoBoletim - 1).getNotas().isEmpty()) {
+            System.out.println("Aluno não possui notas cadastradas!");
+            return;
+        }
+
+        ArrayList<Double> notas = alunos.get(alunoBoletim - 1).getNotas();
+
+        System.out.println("---------------");
+        System.out.println(alunoBoletim + " - " + alunos.get(alunoBoletim - 1).getNome());
+
+        for (int i = 0; i < notas.size(); i++) {
+            System.out.println("Nota " + (i + 1) + ": " + notas.get(i));
+        }
+
+        System.out.println("---------------");
     }
 
     public void removerAluno(int removerAluno) {
-        if (removerAluno <= ( alunos.size()) && removerAluno > 0 && removerAluno != 0) {
-            alunos.remove(alunos.get(removerAluno - 1));
+
+        if (existeAluno(removerAluno)) {
+            alunos.remove(removerAluno - 1);
             System.out.println("Aluno removido com sucesso!");
         } else {
             System.out.println("Aluno não existe!");
@@ -73,18 +93,10 @@ public class Turma {
     }
 
     public boolean existeAluno(int qualAluno) {
-        if (qualAluno <= ( alunos.size()) && qualAluno > 0 && qualAluno != 0){
-            return true;
-        } else {
-            return false;
-        }
+        return qualAluno >= 1 && qualAluno <= alunos.size();
     }
 
     public boolean estaVazia() {
-        if (alunos.isEmpty()){
-            return true;
-        } else {
-            return false;
-        }
+        return alunos.isEmpty();
     }
 }
